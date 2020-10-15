dd6ebcb0ad73b810f9eb5361db7dc58bba30d31b
John Chu <42362023+twmphoto@users.noreply.github.com>
2020-10-15 15:32:52 -0700

    Update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -7,7 +7,7 @@ module.exports = {
       },
     ],
     "plugins": [
-      "@babel/transform-runtime",
+      "@babel/plugin-transform-runtime",
       "@babel/plugin-proposal-class-properties"
     ],
   ],
